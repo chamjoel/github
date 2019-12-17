c5d5c6a982dc9067a2c6d0266f700e5278be1030
Joel-Chambergo <chambergo.joel@iisgalvanimi.edu.it>
2019-12-17 19:55:49 +0000

    punt2

--- chambergo/src/app/container/container.component.ts
+++ chambergo/src/app/container/container.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./container.component.css']
 })
 export class ContainerComponent implements OnInit {
-
-  constructor() { }
+ cantiere: String[];
+  constructor() {
+this.cantiere=[ 'macchina', 'macchina2','macchina3','macchina4','macchina5','macchina6','macchina7','macchina8','macchina9','macchina10']
+   };
 
   ngOnInit() {
   }
